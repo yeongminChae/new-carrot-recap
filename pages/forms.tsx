1791c47af4e89e3c48f582376d857481485886d7
Yeongmin_Chae <codudals98@naver.com>
2023-02-14 17:44:20 +0900

    How to validate the user's data , and how to make errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
 // wishes
 // less code ✅
@@ -8,24 +8,48 @@ import { useForm } from "react-hook-form";
 // don't deal with events ✅
 // easier inputs ✅
 
+interface ILofinForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
 export default function Forms() {
-  const { register, watch } = useForm();
+  const { register, watch, handleSubmit } = useForm<ILofinForm>({});
+  const onValid = (data: ILofinForm) => {
+    console.log("i'm valid");
+  };
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: "username is required",
+          minLength: {
+            value: 5,
+            message: "more than 5 chars",
+          },
+        })}
         // what is '...'? it will create smth like name, onChange...,
         // so it'll takes object and put all the objects inside of props.
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", {
+          required: "email is required",
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", {
+          required: "password is required",
+        })}
         type="password"
         placeholder="password"
-        required
       />
       <input type="submit" value="create Account " />
     </form>
