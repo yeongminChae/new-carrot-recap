31e5bdf89286c5c782bd728ec79797ef8185f767
Yeongmin_Chae <codudals98@naver.com>
2023-02-17 16:03:19 +0900

    twilio and sendgrid for authentication

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { IResponseType } from "@/libs/server/withHandler";
 import client from "@/libs/server/client";
+import twilio from "twilio";
+import mail from "@sendgrid/mail";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+mail.setApiKey(process.env.SENDGRID_KEY!);
 
 async function handler(
   req: NextApiRequest,
@@ -27,7 +32,22 @@ async function handler(
     },
   });
   console.log(token);
-
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload} `,
+    });
+    console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "codudals98@naver.com",
+      to: "codudals98@naver.com",
+      subject: "Your carror market verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+  }
   return res.json({
     ok: true,
   });
