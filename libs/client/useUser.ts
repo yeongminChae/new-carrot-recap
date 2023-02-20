24c34f986c14075803f0b4248a1eca90ac5d337f
Yeongmin_Chae <codudals98@naver.com>
2023-02-20 16:59:51 +0900

    deep dive for useSWR and cleaning codes

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,17 +1,20 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import useSWR from "swr";
-
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 // const fetcher = (url: string) => {
 //   fetch(url).then((response) => response.json());
 // }; i used curly braces so i need to write return.
 
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me", fetcher);
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
-
-  return data;
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+  return { user: data?.profile, isLoading: !data && !error };
 }
 
 // without useSWR
