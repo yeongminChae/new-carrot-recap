8d0cc0d897919dc9265e6229b8004f4d2c36ee8a
Yeongmin_Chae <codudals98@naver.com>
2023-02-20 16:48:55 +0900

    useUser hook modifying with useSWR

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,18 +1,34 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
+
+// const fetcher = (url: string) => {
+//   fetch(url).then((response) => response.json());
+// }; i used curly braces so i need to write return.
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me", fetcher);
   const router = useRouter();
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter"); // replace and push r similiar but replace donsn't allow u to have history.
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+
+  return data;
 }
+
+// without useSWR
+
+// import { useEffect, useState } from "react";
+// export default function useUser() {
+//     const [user, setUser] = useState();
+//     const router = useRouter();
+//     useEffect(() => {
+//       fetch("/api/users/me")
+//         .then((response) => response.json())
+//         .then((data) => {
+//           if (!data.ok) {
+//             return router.replace("/enter"); // replace and push r similiar but replace donsn't allow u to have history.
+//           }
+//           setUser(data.profile);
+//         });
+//     }, [router]);
+//     return user;
+//   }
